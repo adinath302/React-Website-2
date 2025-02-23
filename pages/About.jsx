9b169bb8384343a54151ab68ce510d03b8bc7252
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2025-02-23 21:22:37 +0530

    Add files via upload

---
+++ pages/About.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section-about container'>
+      <h2 className='container-title'>
+        Here are the Interesting Facts
+        <br />
+        we're proud of
+      </h2>
+      <div className='gradient-cards'>
+        <div className='card'>
+          <div className='container bg-blue-box'>
+            <p className='card-title'>India</p>
+            <p>
+              <span className="card-description">Capital: </span>
+              Delhi
+            </p>
+            <p>
+              <span className="card-description">Population: </span>
+              Delhi
+            </p>
+            <p>
+              <span className="card-description">IntrestingFacts: </span>
+              Delhi
+            </p>
+            <p>
+              <span className="card-description">Capital: </span>
+              Delhi
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
